21122edbab830af0b1966453bdd779d2ed74b33a
LIghtJUNction <2418701971@qq.com>
2024-12-12 21:34:11 +0800

    fix

--- MyModule/service.sh
+++ MyModule/service.sh
@@ -1,12 +1,14 @@
 #!/system/bin/sh
+MODDIR=${0%/*}
+source $MODDIR/tools.sh # 导入工具函数
 
 until [ "$(getprop sys.boot_completed)" = "1" ]; do
     echo "等待开机完成"
     sleep 1
 done
+set_module_description "模块将在20秒后启动"
+sleep 20
 
-MODDIR=${0%/*}
-source $MODDIR/tools.sh # 导入工具函数
 log INFO "开机运行"
 crond 1
 $MODDIR/UniCron.sh
