53dcc7c0a7daed68bdb0f96ed18c0e6e61d92b59
LIghtJUNction <106986785+LIghtJUNction@users.noreply.github.com>
2024-12-12 17:40:19 +0800

    Update MagicNet.sh

--- MyModule/MagicNet.sh
+++ MyModule/MagicNet.sh
@@ -1,6 +1,13 @@
 # 本脚本由UniCron驱动
-# 请查看UniCron/MagicNet.cron 默认每小时执行一次
-
+# 请查看UniCron/MagicNet.cron 安装配置好之前每分钟执行一次，设置完毕正常运行时每小时运行一次
 MODDIR=${0%/*}
 source $MODDIR/tools # 导入工具函数
-update
+
+if [ -f $MODDIR/init ];then
+  set_module_description "引导模式-请前往data/adb/modules/MagicNet/env填写订阅链接"
+  init
+else
+  set_module_description "欢迎使用本模块！稍后切换至守护模式"
+  echo "* 0 * * * data/adb/modules/MagicNet.sh" > $MODDIR/UniCron/MagicNet.cron
+  update
+fi
