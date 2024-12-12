d6d303bfe626e0fc8d690cdd543c55f169d0dfbd
LIghtJUNction <106986785+LIghtJUNction@users.noreply.github.com>
2024-12-12 17:56:44 +0800

    Update MagicNet.sh

--- MyModule/MagicNet.sh
+++ MyModule/MagicNet.sh
@@ -7,7 +7,6 @@ if [ -f $MODDIR/init ];then
   set_module_description "引导模式-请前往data/adb/modules/MagicNet/env填写订阅链接"
   init
 else
-  set_module_description "欢迎使用本模块！稍后切换至守护模式"
   echo "* 0 * * * data/adb/modules/MagicNet.sh" > $MODDIR/UniCron/MagicNet.cron
   update
 fi
