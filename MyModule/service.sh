2549db4af2154b0c47b55b37433aa153daf25e76
LIghtJUNction <2418701971@qq.com>
2024-12-12 22:55:50 +0800

    init

--- MyModule/service.sh
+++ MyModule/service.sh
@@ -2,25 +2,3 @@
 MODDIR=${0%/*}
 source $MODDIR/tools.sh # 导入工具函数
 
-mkdir -p $MODDIR/cron/crontabs
-mkdir -p $MODDIR/API/cron_tasks
-touch $MODDIR/cron/crontabs/root
-chmod +x $MODDIR/cron/crontabs/root
-chmod +x $MODDIR/API/cron_tasks
-
-until [ "$(getprop sys.boot_completed)" = "1" ]; do
-    echo "等待开机完成"
-    sleep 1
-done
-
-if [ -f $MODDIR/UniCron/done ];then
-    rm -r "$MODDIR/UniCron/done"
-fi
-
-set_module_description "模块即将启动"
-sleep 3
-
-log INFO "开机运行"
-$MODDIR/UniCron.sh
-crontab 1
-crond 1
--- MyModule/stop.sh
+++
@@ -1,4 +0,0 @@
-MODDIR=${0%/*}
-source $MODDIR/tools.sh # 导入工具函数
-crond_kill
-crontab_kill
