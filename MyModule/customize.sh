ed1159616f79fe38d39709ecc5d71e735433723e
LIghtJUNction <lightjunction.me@gmail.com>
2025-01-22 23:58:42 +0800

    优化观感

--- MyModule/customize.sh
+++ MyModule/customize.sh
@@ -113,7 +113,8 @@ detect_key_press() {
   done
 }
 
-ui_print "跳转GitHub仓库给作者一个star（音量+）；取消跳转（音量-）--默认跳转"
+ui_print "跳转GitHub仓库给作者一个star（音量+）--默认；"
+ui_print "取消跳转（音量-)"
 case $(detect_key_press) in
   0)
     ui_print "- No input detected after 10 seconds -- 10秒后没有输入_默认跳转"
@@ -128,7 +129,8 @@ case $(detect_key_press) in
     ;;
 esac
 
-ui_print "是否使用sub_store模块?（音量+）；否（音量-）--默认否"
+ui_print "是否使用sub_store模块?（音量+）；"
+ui_print "否（音量-）--默认"
 case $(detect_key_press) in
   0)
     ui_print " 超时-不启用sub_store "
@@ -139,11 +141,12 @@ case $(detect_key_press) in
     ;;
   2)
     ui_print " 不启用sub_store，请手动填写url "
-    rm -f $MODPATH/magic_sub || ui_print "好的，不使用magic_sub"
+    rm -f $MODPATH/magic_sub || ui_print "好的，不使用ma"gic_sub"
     
 esac
 
-echo "使用yacd？（音量+）；否（音量-）--默认否-使用官方默认配置"
+ui_print "使用yacd？（音量+）；"
+ui_print "否（音量-）--默认     使用官方默认配置"  
 case $(detect_key_press) in
   0)
     ui_print " 超时-使用官方默认 "
