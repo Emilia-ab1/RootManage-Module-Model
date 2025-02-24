4cdd9720c1bdc608ffc7b49e6110a6b6354d447f
LIghtJUNction <lightjunction.me@gmail.com>
2025-02-24 23:07:10 +0800

    Update action.sh

--- MyModule/action.sh
+++ MyModule/action.sh
@@ -1,10 +1,6 @@
 # 刷新cron配置,并输出当前cron配置
 MODDIR=${0%/*}
 
-SPOOLDIR="$MODDIR/webroot/spool"
-CONFIGDIR="$MODDIR/webroot/etc"
-CRON_LOG="$MODDIR/webroot/cron.log"
-
 $MODDIR/service.sh
 
 echo "刷新完成"
