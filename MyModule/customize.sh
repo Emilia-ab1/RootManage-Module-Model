c0ee5910986ca36e294c75eb4dac2bd537a16e98
LIghtJUNction <lightjunction.me@gmail.com>
2025-02-24 23:32:53 +0800

    fix

--- MyModule/customize.sh
+++ MyModule/customize.sh
@@ -16,7 +16,6 @@
 # - set_perm_recursive: 递归设置目录及其内容的权限。
 
 ui_print "欢迎使用本前置模块"
-ui_print "版本4.0"
 ui_print "作者：@LIghtJUNction"
 ui_print "github：https://github.com/LIghtJUNction/RootManage-Module-Model/blob/UniCron"
 
